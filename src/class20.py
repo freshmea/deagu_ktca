6836bfc1a00b547335ba246d91762f6efe097792
freshmea <freshmea@naver.com>
2023-05-08 17:10:44 +0900

    aa

---
+++ src/class20.py
@@ -0,0 +1,16 @@
+def main():
+    color_list=['red', 'yellow', 'white', 'blue', 'orange', 'green', 'navy', 'purple']
+    for i, value in enumerate(color_list):
+        print( i,'번째 변수는 :' ,value)
+    
+    dict_a = {'choi': 170,
+              'kim': 165,
+              'lee': 180,
+              'su': 150,
+              'park': 177}
+    
+    for key, value in dict_a.items():
+        print(key,'키의 값은 : ', value)
+
+if __name__ == '__main__':
+    main()
---
+++ src/example1.py
@@ -0,0 +1,16 @@
+# 무작위 로또 변호 출력하기
+import random
+
+def main():
+    number_list = list()
+    i = 0
+    while i < 6:
+        number = random.randint(1, 45)
+        if number in number_list:
+            continue
+        number_list.append(number)
+        i += 1
+    print(number_list)
+
+if __name__ == '__main__':
+    main()
