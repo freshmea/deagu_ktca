3096fe92833a05cc5d72e69240355b1141b219bf
freshmea <freshmea@naver.com>
2023-05-09 13:47:05 +0900

    aa

---
+++ src/class33.py
@@ -0,0 +1,13 @@
+def generater():
+    print("함수가 호출 되었습니다.")
+    yield "test"
+
+def main():
+    print('A 지점 통과')
+    generater()
+    print('B 지점 통과')
+    generater()
+    print(generater())
+
+if __name__ == '__main__':
+    main()
---
+++ src/class34.py
@@ -0,0 +1,25 @@
+def generater():
+    print("A 지점 통과")
+    yield 1
+    print("B 지점 통과")
+    yield 2
+    print("C 지점 통과")
+    yield 3
+
+def main():
+    output = generater()
+    # print('D 지점 통과')
+    # a = next(output)
+    # print(a)
+    
+    # print('E 지점 통과')
+    # b = next(output)
+    # print(b)
+    
+    # c = next(output)
+    # print(c)
+    for i in output:
+        print('aa')
+        print(i)
+if __name__ == '__main__':
+    main()
