f149c32580abf11ad64f763142d9dbffcf94b811
freshmea <freshmea@naver.com>
2023-05-10 16:12:41 +0900

    test

--- pygame/first_game.py
+++ pygame/first_game.py
@@ -1,3 +1,4 @@
+from typing import Any
 import pygame
 import sys
 
@@ -9,18 +10,21 @@ class Ball(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self, self.groups)
         self.size = size
         self.image = pygame.Surface((self.size * 2, self.size * 2))
-        pygame.draw.circle(self.image, (100,100,100), (self.size, self.size), self.size)
+        pygame.draw.circle(self.image, (1,0,0), (self.size, self.size), self.size)
         self.image.set_colorkey((0, 0, 0))
         self.rect = self.image.get_rect()
         self.pos = pos
         self.all_sprites = pygame.sprite.Group()
-        
+    
+    def update(self):
+        self.rect.center = self.pos
+        self.pos = self.pos + vec(1,0)
 
 pygame.init()
 screen = pygame.display.set_mode((600, 300))
 clock = pygame.time.Clock()
 all_sprites = pygame.sprite.Group()
-ball = Ball(all_sprites, vec(100,100), 10 )
+ball = Ball(all_sprites, vec(200,100), 10 )
 all_sprites.add(ball)
 
 while True:
@@ -29,9 +33,9 @@ while True:
             sys.exit()
     clock.tick(60)
     color = (255,255,255)
-    
     screen.fill(color)
-    ball.update()
+    
+    all_sprites.update()
     all_sprites.draw(screen)
     pygame.display.update()
     
